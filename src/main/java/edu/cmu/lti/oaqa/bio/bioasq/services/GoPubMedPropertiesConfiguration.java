06351abcee1bba1cccef0e974a8c5a5aa31cb6ef
ziy <sincerezi@gmail.com>
2015-02-14 23:48:23 -0500

    Enabled to reuse session url before a timeout.

--- src/main/java/edu/cmu/lti/oaqa/bio/bioasq/services/GoPubMedPropertiesConfiguration.java
+++ src/main/java/edu/cmu/lti/oaqa/bio/bioasq/services/GoPubMedPropertiesConfiguration.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
@@ -17,6 +20,8 @@ public class GoPubMedPropertiesConfiguration {
 
   public static final String REQUIRE_SESSION_URL = "require.session.url";
 
+  public static final String SESSION_REFRESH_INTERVAL = "session.refresh.interval";
+
   public GoPubMedPropertiesConfiguration(String gopubmedPropertiesFilepath)
           throws ConfigurationException {
     this.config = new PropertiesConfiguration(gopubmedPropertiesFilepath);
@@ -29,10 +34,33 @@ public class GoPubMedPropertiesConfiguration {
   public String getUrl(GoPubMedServiceKey service) throws MalformedURLException, IOException {
     String serviceUrl = config.getString(service.getKey());
     if (config.getBoolean(REQUIRE_SESSION_URL)) {
-      return CharStreams.toString(new InputStreamReader(new URL(serviceUrl).openStream()));
+      return timeout(serviceUrl) ? updateSessionUrl(serviceUrl) : getSessionUrl(serviceUrl);
     } else {
       return serviceUrl;
     }
+  }
+
+  private Map<String, String> service2session = new HashMap<>();
+
+  private Map<String, Calendar> service2timeout = new HashMap<>();
+
+  private boolean timeout(String serviceUrl) {
+    return !service2timeout.containsKey(serviceUrl)
+            || Calendar.getInstance().after(service2timeout.get(serviceUrl));
+  }
+
+  private String updateSessionUrl(String serviceUrl) throws MalformedURLException, IOException {
+    String sessionUrl = CharStreams
+            .toString(new InputStreamReader(new URL(serviceUrl).openStream()));
+    service2session.put(serviceUrl, sessionUrl);
+    Calendar timeout = Calendar.getInstance();
+    timeout.add(Calendar.MINUTE, config.getInt(SESSION_REFRESH_INTERVAL, 9));
+    service2timeout.put(serviceUrl, timeout);
+    return sessionUrl;
+  }
+
+  private String getSessionUrl(String serviceUrl) {
+    return service2session.get(serviceUrl);
   }
 
 }
